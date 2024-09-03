9a254131f97cb78491a0cc893a0e42ba6ca90f50
kauephe <165967721+kauephe@users.noreply.github.com>
2024-09-03 22:57:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "Kaue";
+const sobreNome = "Phelipe";
+const idade = 19;
+const cidadeNatal = "paranaguá";
+const altura = 1.75
+
+console.log("o meu prineiro nome é");
+console.log(primeiroNome);
+console.log("meu sobre nome é");
+console.log(sobreNome);
+console.log("minha idade é");
+console.log(idade);
+console.log("minha cidade natal é");
+console.log(cidadeNatal);
+console.log("minha altura é");
+console.log(altura);
